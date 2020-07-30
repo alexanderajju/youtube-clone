b2acf915a1164db13c15f16ac42045d6079686b6
alexanderajju <alexanderajju@gmail.com>
2020-07-30 11:53:14 +0530

    selected class added

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './SidebarRow.css'
-import { Icon } from '@material-ui/core';
+import React from "react";
+import "./SidebarRow.css";
+import { Icon } from "@material-ui/core";
 
-
-function SidebarRow({Icon,title}) {
-    return (
-        <div className="sidebarRow">
-            <Icon className="sidebarRow__icon" />
-            <h2 className="sidebarRow__title">{title}</h2>
-        </div>
-    )
+function SidebarRow({ selected, Icon, title }) {
+  return (
+    <div className={`sidebarRow ${selected && "selected" }` }>
+      <Icon className="sidebarRow__icon " />
+      <h2 className="sidebarRow__title">{title}</h2>
+    </div>
+  );
 }
 
-export default SidebarRow
+export default SidebarRow;
