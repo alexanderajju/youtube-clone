2303a230e6a2f5a70a774246714769772ce3150f
alexanderajju <alexanderajju@gmail.com>
2020-07-30 18:43:26 +0530

    React Route added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar'
-import RecommentedVideos from './RecommentedVideos';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import RecommentedVideos from "./RecommentedVideos";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-     
-      <Header/>
-
-      <div className="app__page">
-      <Sidebar/>
-      <RecommentedVideos/>
-      </div>
-      
-
-
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <h1>Search Page</h1>
+          </Route>
+          <Route path="/">
+            <div className="app__page">
+              <Sidebar />
+              <RecommentedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
