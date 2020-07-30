a8c3172e33cda183bb82eaeb1bde718f4f62608e
alexanderajju <alexanderajju@gmail.com>
2020-07-30 20:30:58 +0530

    Video Row done

--- src/VideoRow.js
+++ src/VideoRow.js
@@ -10,10 +10,18 @@ function VideoRow({
   image,
   title,
 }) {
-  return <div className="videoRow">
-
-
-  </div>;
+  return (
+    <div className="videoRow">
+      <img src={image} alt="" />
+      <div className="videoRow__text">
+        <h3>{title}</h3>
+        <p className="videoRow__headline">
+          {channel}• <span className="videoRow__subs" ><span className="videoRow__subsNumber" >{subs}</span> subscribers </span>{views} • {timestamp}{" "}
+        </p>
+        <p className="videoRow__description">{description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default VideoRow;
