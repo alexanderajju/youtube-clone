2baddc3cf23043f81354f65ee3810c621604f298
alexanderajju <alexanderajju@gmail.com>
2020-07-30 19:01:19 +0530

    Search Box funtion added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/search">
+          <Route path="/search/:searchTerm">
             <h1>Search Page</h1>
           </Route>
           <Route path="/">
--- src/Header.js
+++ src/Header.js
@@ -1,41 +1,53 @@
-import React from 'react';
-import './Header.css';
-import VideoCallSharpIcon from '@material-ui/icons/VideoCallSharp';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AppsIcon from '@material-ui/icons/Apps'
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import Avatar from '@material-ui/core/Avatar'
-
+import React, { useState } from "react";
+import "./Header.css";
+import VideoCallSharpIcon from "@material-ui/icons/VideoCallSharp";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar";
+import {Link   } from "react-router-dom"
 
 function Header() {
-    return (
-        <div className="header">
-            <div className="header__left">
-            <MenuIcon/>
-            <img className="header__logo"
-             src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt=""/>
-            </div>
-            
-            <div className="header__input">
-            <input placeholder="Search"  type="text"/>
+  const [input, setInput] = useState("");
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+          alt=""
+        />
+      </div>
+
+      <div className="header__input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Search"
+          type="text"
+        />
+        <Link to={`/search/${input}`}>
+
         <SearchIcon className="header__inputButton" />
-            </div>
-       
-<div className="header__icons">
-<VideoCallSharpIcon className="header__icon"/>
+        </Link>
         
-        <AppsIcon className="header__icon"/>
-        <NotificationsIcon className="header__icon"/>
+      </div>
+
+      <div className="header__icons">
+        <VideoCallSharpIcon className="header__icon" />
+
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
 
         <Avatar
-        alt="Aju Alex"
-        src="https://yt3.ggpht.com/a-/AOh14Ghyd7X3-yGlE6I7ylN9ljC4_rHF3Be4X9EvwdfxYqM=s88-c-k-c0xffffffff-no-rj-mo"
+          alt="Aju Alex"
+          src="https://yt3.ggpht.com/a-/AOh14Ghyd7X3-yGlE6I7ylN9ljC4_rHF3Be4X9EvwdfxYqM=s88-c-k-c0xffffffff-no-rj-mo"
         />
-</div>
-       
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
