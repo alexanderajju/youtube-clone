5e46d11132423c005ebe89aceda14d4c90da3362
alexanderajju <alexanderajju@gmail.com>
2020-07-30 19:03:24 +0530

    youtube Logo Route added

--- src/Header.js
+++ src/Header.js
@@ -14,11 +14,15 @@ function Header() {
     <div className="header">
       <div className="header__left">
         <MenuIcon />
+
+        <Link to="/">
         <img
           className="header__logo"
           src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
           alt=""
         />
+        </Link>
+       
       </div>
 
       <div className="header__input">
